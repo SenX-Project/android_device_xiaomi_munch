fbbdd3001ad0b5154ffadbb03e43f4f24fcfc637
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-23 22:43:52 +0100

    munch: Readd build description override
    Change-Id: Ib5c54bb5babe928196b83da5d9b9945c5f6fff13
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- lineage_munch.mk
+++ lineage_munch.mk
@@ -23,4 +23,5 @@ PRODUCT_MODEL := 22021211RG
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="munch-user 13 RKQ1.211001.001 V816.0.8.0.ULMMIXM release-keys" \
     BuildFingerprint=POCO/munch_global/munch:13/RKQ1.211001.001/V816.0.8.0.ULMMIXM:user/release-keys
